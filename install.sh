df97150d744186c8d9dccc4e89a8e5c665164aba
Jeffrey-David Kapp <jkapp@opennms.com>
2022-03-23 10:30:24 -0400

    OP-38: install dependency helm charts separately and manually

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#use this script to install a basic version of the opennms operator
+
+# cert-manager
+helm repo add jetstack https://charts.jetstack.io
+
+helm upgrade -i \
+  cert-manager jetstack/cert-manager \
+  --namespace onms-cert-manager \
+  --create-namespace \
+  --version v1.7.0 \
+  --set installCRDs=true
+
+# operator
+helm repo add opennms-operator https://opennms.github.io/opennms-operator/charts/packaged
+
+helm upgrade -i opennms-operator opennms-operator/opennms-operator -f values.yaml
+
+# k8s replicator
+helm repo add mittwald https://helm.mittwald.de
+
+helm upgrade -i kubernetes-replicator --namespace opennms mittwald/kubernetes-replicator
